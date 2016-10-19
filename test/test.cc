91bc4769382869b96ef7bf9f0d574237675ddf6d
Michael Catanzaro <michael.catanzaro@mst.edu>
2016-10-18 22:17:04 -0500

    Fix modelines
    Guess I was never pressing tab today?

--- test/test.cc
+++ test/test.cc
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
  * Copyright (c) 2016 Michael Catanzaro <michael.catanzaro@mst.edu>
  *
