86ae1457a21f7a1b4b3c1fd579c13b164d717137
Michael Catanzaro <michael.catanzaro@mst.edu>
2016-10-21 17:57:36 -0500

    Add partial solutions to homework problems

--- src/main.cc
+++ src/main.cc
@@ -41,9 +41,62 @@ using namespace multinv;
  * Reference: https://en.wikipedia.org/wiki/Finite_field_arithmetic
  */
 int main() {
-    // From homework 3 problem 3
-    uint16_t ip = 0b0000000000001011;
-    Polynomial p{0b00000010, ip, 3};
-    Polynomial result = multiplicative_inverse(p);
-    std::printf("Multiplicative inverse of 0x%x with IP 0x%x is 0x%x\n", p.value(), ip, result.value());
+    // TODO: Implement division?
+    // * Cannot do problem 1 without it
+    // * Required for complete answers to problem 2 and problem 4
+
+    // Homework 3 problem 2
+    // Default characteristic and IP are large enough to avoid mod operations.
+    {
+        Polynomial p1{0b101111};
+        Polynomial p2{0b111};
+        std::printf("Homework 3 problem 2\n");
+        std::printf("%x + %x = %x\n", p1.value(), p2.value(), (p1 + p2).value());
+        std::printf("%x - %x = %x\n", p1.value(), p2.value(), (p1 - p2).value());
+        std::printf("%x * %x = %x\n", p1.value(), p2.value(), (p1 * p2).value());
+        std::printf("\n");
+    }
+
+    // Homework 3 problem 3
+    {
+        uint16_t ip1 = 0b1011;
+        uint16_t ip2 = 0b1101;
+        Polynomial p1{0b00000010, ip1, 3};
+        Polynomial r1 = multiplicative_inverse(p1);
+        Polynomial p2{0b00000010, ip2, 3};
+        Polynomial r2 = multiplicative_inverse(p2);
+        std::printf("Homework 3 problem 3\n");
+        std::printf("Multiplicative inverse of %x with IP %x is %x\n", p1.value(), ip1, r1.value());
+        std::printf("Multiplicative inverse of %x with IP %x is %x\n", p2.value(), ip2, r2.value());
+        std::printf("\n");
+    }
+
+    // Homework 3 problem 4
+    {
+        Polynomial p1{0b111, 0b1011, 3};
+        Polynomial p2{0b101, 0b1011, 3};
+        Polynomial p3{0b111, 0b1101, 3};
+        Polynomial p4{0b101, 0b1101, 3};
+
+        std::printf("Homework 3 problem 4\n");
+        std::printf("With irreducible polynomial 1011:\n");
+        std::printf("%x + %x = %x\n", p1.value(), p2.value(), (p1 + p2).value());
+        std::printf("%x - %x = %x\n", p1.value(), p2.value(), (p1 - p2).value());
+        std::printf("%x * %x = %x\n", p1.value(), p2.value(), (p1 * p2).value());
+        std::printf("With irreducible polynomial 1101:\n");
+        std::printf("%x + %x = %x\n", p3.value(), p4.value(), (p3 + p4).value());
+        std::printf("%x - %x = %x\n", p3.value(), p4.value(), (p3 - p4).value());
+        std::printf("%x * %x = %x\n", p3.value(), p4.value(), (p3 * p4).value());
+        std::printf("\n");
+    }
+
+    // Homework 3 problem 5
+    {
+        Polynomial p1{0b10101101};
+        Polynomial p2{0b1101};
+        std::printf("Homework 3 problem 5\n");
+        std::printf("Multiplicative inverse of %x is %x\n", p1.value(), multiplicative_inverse(p1).value());
+        std::printf("Multiplicative inverse of %x is %x\n", p2.value(), multiplicative_inverse(p2).value());
+        std::printf("\n");
+    }
 }
